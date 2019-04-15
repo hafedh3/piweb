27b0cc891dc1987958df147429b027dc9a3aa5b1
HelmiBellahirech <35872252+HelmiBellahirech@users.noreply.github.com>
2019-04-15 06:24:10 +0200

    commit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new AnnonceBundle\AnnonceBundle(),
             new EventBundle\EventBundle(),
             new CommandeBundle\CommandeBundle(),
+            new SBC\NotificationsBundle\NotificationsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
